2bd72ef4517f1573d1bd7038c7aa8731d8bddfe1
Sergio <ser.alvarez1998@gmail.com>
2023-12-26 14:44:27 -0300

    dotnet files

---
+++ GameBacklog/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace GameBacklog
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        private void MainWindowStartUp(object sender, StartupEventArgs e)
+        {
+            var wnd = new MainWindow
+            {
+                Title = "Game backlog",
+                Width = 500,
+                Height = 350
+            };
+            wnd.Show();
+        }
+    }
+}
---
+++ GameBacklog/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Globalization;
+using System.Windows;
+using System.Windows.Input;
+using GameBacklog.controllers;
+
+namespace GameBacklog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private readonly GridClickController _controller;
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            _controller = new GridClickPrinterController();
+        }
+        
+        private void pnlMainGrid_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            _controller.handle_click(new System.Collections.Generic.Dictionary<string, string>
+            {
+                { "x", e.GetPosition(pnlMainGrid).X.ToString(CultureInfo.InvariantCulture) },
+                { "y", e.GetPosition(pnlMainGrid).Y.ToString(CultureInfo.InvariantCulture) }
+            });
+            
+            MessageBox.Show("You clicked me at " + e.GetPosition(this).ToString());
+        }
+
+    }
+}
